37ec32bb4f52961f81d66a56e51d83dd5f939277
subrat37 <subratsahoo9420@gmail.com>
2022-04-16 01:45:42 +0530

    Database and server setup

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,24 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 
+// variables
+const app = express();
+
 // local packages requiring
+const connectDB = require("./utils/connectDB");
+
+// global middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// start func
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI).then(() => console.log("Database connected"));
+    app.listen(process.env.PORT || 5000, () => console.log("Server running"));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
